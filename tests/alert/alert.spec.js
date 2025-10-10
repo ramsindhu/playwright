d47069b6b8922d6639f3118e51e646599691ca1d
arvinweb-hub <arvinwebhacker@gmail.com>
2025-10-10 18:57:38 +0530

    test for allure report

---
+++ tests/alert/alert.spec.js
@@ -0,0 +1,73 @@
+import {test} from "@playwright/test";
+
+test("Alert Handling", async({page})=>{
+    await page.goto("https://demo.automationtesting.in/Alerts.html");
+
+    await page.once("dialog", async(dialog)=>{
+console.log("Simple Alert", dialog.message());
+await dialog.accept();
+    });
+    const simpleAlert = await page.locator("//button[@onclick='alertbox()']")
+   await simpleAlert.click();
+
+
+const confirmAlert = await page.locator("//a[text()='Alert with OK & Cancel ']");
+await confirmAlert.click();
+
+
+await page.once("dialog", async(dialog)=>{
+    console.log("Confirm ALert", dialog.message());
+    await dialog.dismiss();
+
+});
+    const clickbtn = await page.locator("//button[@onclick='confirmbox()']");
+await clickbtn.click();
+
+const promptAlert = await page.locator("//a[text()= 'Alert with Textbox ']")
+await promptAlert.click();
+
+await page.once("dialog", async(dialog)=>{
+    console.log("Prompt Message : ", dialog.message());
+    console.log("Prompt default value : ", dialog.defaultValue());
+     await dialog.accept("Prompt");
+});
+
+const promptBtn = await page.locator("//button[contains(text(), 'prompt box')]");
+await promptBtn.click();
+
+ //added by Ram
+    // console.log("Prompt Message : ", dialog.message());
+    // // console.log("Prompt default value : ", dialog.defaultValue());
+    // //  await dialog.accept("Prompt");
+    // //  console.log("Prompt Message : ", dialog.message());
+    // // console.log("Prompt default value : ", dialog.defaultValue());
+    // //  await dialog.accept("Prompt");
+    // //  console.log("Prompt default value : ", dialog.defaultValue());
+    // //  await dialog.accept("Prompt");
+});
+
+
+
+
+ //if didn't giving accept playwright auotomatically cancel 
+
+
+ //changes by Ram
+//  const promptBtn = await page.locator("//button[contains(text(), 'prompt box')]");
+// await promptBtn.click();
+// const check = await page.locator("//button[contains(text(), 'prompt box')]");
+// await check.click();
+
+const promptBtn = await page.locator("//button[contains(text(), 'prompt box')]");
+await promptBtn.click();
+const check = await page.locator("//button[contains(text(), 'prompt box')]");
+await check.click();
+
+
+ 
+
+
+
+
+
+ 
